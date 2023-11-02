01c5ae3f06543dc70d1c1ccc3b4882d136e09031
Erlone_alpha <125753378+Erlonealpha@users.noreply.github.com>
2023-11-02 09:30:07 +0800

    add print

--- python_sklearn/logistic/load_breast_cancer.py
+++ python_sklearn/logistic/load_breast_cancer.py
@@ -24,3 +24,30 @@ lr_model = LogisticRegression(solver='saga')
 # 训练Logistic回归模型
 lr_model.fit(x_trainStd, y_train)
 print('训练出来的LogisticRegression模型为: \n', lr_model)
+
+#查看模型相关特征
+print('各特征的相关系数为：\n', lr_model.coef_)
+
+print('模型的截距为：', lr_model.intercept_)
+
+print('模型的迭代次数为：', lr_model.n_iter_)
+
+print('预测测试集前10个结果为：\n', lr_model.predict(x_testStd)[: 10])
+
+print('测试集准确率为：', lr_model.score(x_testStd, y_test))
+
+print('测试集前3个对应类别的概率为：\n', lr_model.predict_proba(x_testStd)[: 3])
+
+print('测试集前3个对应类别的概率的log值为：\n',
+      lr_model.predict_log_proba(x_testStd)[: 3])
+
+print('测试集前3个的决策函数值为：\n',
+      lr_model.decision_function(x_testStd)[: 3])
+
+print('模型的参数为：\n', lr_model.get_params())
+
+print('修改max_iter参数为1000后的模型为：\n', lr_model.set_params(max_iter=1000))
+
+print('系数矩阵转为密度数组后的模型为：\n', lr_model.densify())
+
+print('系数矩阵转为稀疏形式后的模型为：\n', lr_model.sparsify())
